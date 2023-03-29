552e5acf159b24b3a84e32ee6f655c5ce0350db0
Ds3Dante <dannypinedanieto@hotmail.com>
2023-03-29 17:25:20 -0500

    https://github.com/Ds3Dante/Entrega-de-deberes

---
+++ Ejercicio_1_Pregunta2.py
@@ -0,0 +1,16 @@
+print("EJERCICIO 1 PREGUNTA 2")
+print("CURSO DE FUNDAMENTOS DE PYTHON")
+print("DANNY PINEDA\n")
+
+nombre=input("Escriba su nombre: ")
+print("Opcion 1 para presentar Datos")
+print("Hola "+(nombre))
+
+print("Opcion 2 para presentar Datos" )
+print(f"Hola {nombre}")
+
+print("Opcion 3 para presentar Datos")
+print("Hola {}".format(nombre))
+
+print("Opcion 4 para presentar Datos")
+print("Hola",nombre)
